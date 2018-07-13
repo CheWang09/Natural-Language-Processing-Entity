4b02d0a7934a44139c23b5f0f8bccddefeb1fb69
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2018-07-13 19:15:12 +0800

    Update Neo4jUtils.scala

--- knowledgeExtractor/src/main/scala/com/bj58/opt/zhaopin/nlp/knowledgeExtractor/utils/Neo4jUtils.scala
+++ knowledgeExtractor/src/main/scala/com/bj58/opt/zhaopin/nlp/knowledgeExtractor/utils/Neo4jUtils.scala
@@ -5,6 +5,6 @@ import org.neo4j.driver.v1._
   * Created by Cheney WANG on 2018/7/9.
   */
 object Neo4jUtils {
-  lazy val driver:Driver = GraphDatabase.driver( "bolt://10.126.88.216:7687", AuthTokens.basic( "neo4j", "neo4j" ) )
+  lazy val driver:Driver = GraphDatabase.driver( "bolt://xxx:xxx:xxx:xxxx", AuthTokens.basic( "neo4j", "neo4j" ) )
   lazy val session:Session = driver.session()
 }
